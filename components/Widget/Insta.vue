83385833f70f063dcd5015e6a587ba08e1e3886b
Epistol <mikaleb@live.fr>
2020-09-18 14:57:41 +0200

    fix(instagram): add description

--- components/Widget/Insta.vue
+++ components/Widget/Insta.vue
@@ -1,18 +1,55 @@
 <template>
-  <v-card shaped style="border-radius: 1rem !important" class="mb-8">
+  <v-card shaped style="border-radius: 1rem !important" class="mt-4 mb-6">
     <template>
-      <a href="https://cuisinedegeek.com/shop">
-        <v-card-title class="font-weight-bold">{{ $t('common.ads.instagram') }}</v-card-title>
+      <a href="https://instagram.com/CuisineDeGeek" target="_blank">
+        <v-card-title class="font-weight-bold">
+          <v-icon class="pr-4">fab fa-instagram</v-icon>
+          {{ $t('common.ads.instagram') }}
+        </v-card-title>
       </a>
       <v-card-text style="min-height:150px;min-width:150px">
         <v-container fluid>
+          <div class="pl-2">
+            <v-avatar>
+              <img src="/icon.png" alt="CDG Profile Instagram" />
+            </v-avatar>
+
+            <v-btn
+              class="ml-4 ma-2"
+              tile
+              color="indigo"
+              dark
+              href="https://instagram.com/CuisineDeGeek"
+              target="_blank"
+            >
+              <i class="pr-1 fab fa-instagram"></i>
+              <span>{{$t('common.ads.instagram')}}</span>
+            </v-btn>
+            <p class="mt-2">
+              CDG: Popculture Recipes
+              <br />🍧 Food Pics Sharing - #popculture #recipes #geekfood
+              <br />🎽🍀 Shop available
+              <br />✈️ Fast worldwide shipping
+              <br />⏬ Recipes - website in desc ( 🇺🇸🇫🇷🇯🇵 )
+              <br />
+            </p>
+          </div>
+
           <v-row>
             <v-col cols="12">
-              <v-row class style="height: 300px;">
+              <v-row class>
                 <div class="ma-3 pa-3" v-for="(n,i) in data" :key="n.id">
-                  <template v-if="i <= 8">
-                    <!-- <instagram-embed :url="n.permalink" :max-width="150" :hide-caption="true" /> -->
-                    <img :src="n.media_url" :width="150" />
+                  <template v-if="i <= 11">
+                    <a :href="n.permalink" target="_blank">
+                      <v-hover v-slot:default="{ hover }">
+                        <v-img
+                          :src="n.media_url"
+                          :height="150"
+                          :width="150"
+                          :class="{ 'on-hover': hover }"
+                        ></v-img>
+                      </v-hover>
+                    </a>
                   </template>
                 </div>
               </v-row>
@@ -27,13 +64,10 @@
 import { defineComponent, ref, onMounted } from '@vue/composition-api'
 import axios from 'axios'
 //@ts-ignore
-import InstagramEmbed from 'vue-instagram-embed'
 
 export default defineComponent({
   name: 'Insta',
-  components: {
-    InstagramEmbed,
-  },
+  components: {},
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setup(props, ctx) {
     let data = ref({})
@@ -50,3 +84,14 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+.v-image {
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0.5;
+}
+
+.v-image:not(.on-hover) {
+  opacity: 1;
+}
+</style>
